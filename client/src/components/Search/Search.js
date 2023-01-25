90d0ecd01d7ed97057ab5a22771055ec0577566d
daniel <gofmandon@gmail.com>
2023-01-25 21:46:47 +0300

    fixed data passed to dataSender

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -5,16 +5,16 @@ import './Search.css'
 const Search = () => {
   const [data, getData] = useState([])
   const dataSender = (input, e) => {
-    const body = JSON.stringify({search: input})
+    const body = JSON.stringify({search: input.target.value})
     const requestHeaders = new Headers({
       "Content-Type": "application/json",
       "Content-Length": JSON.stringify(body).length
     })
-    console.log(JSON.stringify({search: input}))
+    console.log(JSON.stringify({search: input.target.value}))
     ServerAPI_POST({
 			url : "/api/v1/graph/suggestion",
       sendObj: {
-        search: input
+        search: input.target.value
       },
 			onDataReceived : (res) => {
         console.log(res.result);
@@ -24,7 +24,7 @@ const Search = () => {
   }
   return (
       <form className='suggestion'>
-        <input type="text" onChange={(e) => { dataSender(e.target.value);}}/>
+        <input type="text" onChange={(e) => { dataSender(e);}}/>
         <div className='results'>
         {data.map((val, i) => (
           <div  key={i}>
